0c64ec06be2805b26b692a7db454748c39beac24
Ivan Semochkin <ivan.s@7peakssoftware.com>
2024-09-23 23:05:59 +0700

    add store

--- src/lib/stores/Deck.svelte.ts
+++ src/lib/stores/Deck.svelte.ts
@@ -1,5 +1,20 @@
 const SUITES = ['heart', 'spade', 'diamond', 'club'];
-const RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+const RANKS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+const SCORES = {
+	'1': 11,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'10': 10,
+	jack: 10,
+	queen: 10,
+	king: 10
+};
 
 export class Deck {
 	cards = $state([]);
@@ -19,5 +34,45 @@ export class Deck {
 
 	shuffle = () => {
 		this.cards = this.cards.sort(() => Math.random() - 0.5);
+	};
+}
+
+export class Player {
+	hand = $state([]);
+	score = $state(0);
+	canDraw = $derived(() => this.score < 21);
+
+	draw = (card) => {
+		if (this.canDraw) {
+			this.hand.push(card);
+			this.score += SCORES[card.rank];
+		}
+	};
+}
+
+export class Dealer {
+	hand = $state([]);
+	score = $state(0);
+	shouldDraw = $derived(() => this.score < 17);
+
+	draw = (card) => {
+		this.hand.push(card);
+		this.score += SCORES[card.rank];
+	};
+}
+
+export class Game {
+	inGame = $state(false);
+	currentTurn = $state('player');
+	player = $state(new Player());
+	dealer = $state(new Dealer());
+	deck = $state(new Deck());
+
+	restart = () => {
+		this.inGame = false;
+		this.currentTurn = 'player';
+		this.player = new Player();
+		this.dealer = new Dealer();
+		this.deck = new Deck();
 	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,45 @@
 <script lang="ts">
-	import { Deck } from '$lib/stores/Deck.svelte';
+	import { Deck, Player, Dealer, Game } from '$lib/stores/Deck.svelte';
 	import Card from '$lib/components/Card.svelte';
 	import CardsDefinitions from '$lib/Components/CardsDefinitions.svelte';
 
-	const deck = new Deck();
+	const game = new Game();
 </script>
 
 <CardsDefinitions />
 
-<div class="container">
-	<div class="row">
-		<div class="col-12">
-			<h1>Cards</h1>
-		</div>
+<button onclick={() => game.restart()}>Restart</button>
+
+<button disabled={!game.player.canDraw} onclick={() => game.player.draw(game.deck.cards.pop())}
+	>Draw Player</button
+>
+<button onclick={() => game.dealer.draw(game.deck.cards.pop())}>Draw Dealer</button>
+
+<div class="wrapper">
+	Player hand: {game.player.score}
+	<div class="container">
+		{#each game.player.hand as card}
+			<Card name={card.displayName} />
+		{/each}
 	</div>
 
-	{#each deck.cards as card}
-		<Card name={card.displayName} />
-	{/each}
+	Dealer hand: {game.dealer.score}
+	<div class="container">
+		{#each game.dealer.hand as card}
+			<Card name={card.displayName} />
+		{/each}
+	</div>
 </div>
 
 <style>
+	.wrapper {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 	.container {
-		padding: 2rem;
+		display: flex;
 	}
 </style>
